f29a72398e56a632ea3d686bd55aa1bcd6aede48
manimovassagh <mani.mgh@gmail.com>
2021-08-26 01:21:14 +0200

    2

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import React, {ReactElement} from 'react';
-import Mani from './Mani';
+import Mani from './Header/Mani';
+
 
 export default function App(): ReactElement {
   return (
     <div className="ui active inverted dimmer">
-      <Mani/>
+      <Mani color="Red"/>
+      <Mani color="Blue"/>
     </div>
   );
 }
--- src/components/Mani.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactElement } from 'react'
-
-export default function Mani(): ReactElement  {
-  return (
-    <div>
-      <div>Just a New comp</div>   
-    </div>
-  )
-}
