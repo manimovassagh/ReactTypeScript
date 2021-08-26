97ba933a86c7f9db70141e5f9f6687ebbb6abad3
manimovassagh <mani.mgh@gmail.com>
2021-08-26 02:52:10 +0200

    3

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,15 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+import { ChildProcess } from 'child_process';
 import React, {ReactElement} from 'react';
 import Mani from './Header/Mani';
 
 
-export default function App(): ReactElement {
+export default function App(): ReactElement<ChildProcess> {
   return (
     <div className="ui active inverted dimmer">
-      <Mani color="Red"/>
-      <Mani color="Blue"/>
+      <Mani color="Red"></Mani>
+      <Mani color="Blue"></Mani>
+      <Mani color="Blue" >just</Mani>
     </div>
   );
 }
